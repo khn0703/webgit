45dd6bfd1b3e7d1c573890c15cca7581d6e5eee5
hnsky <khn0703@daum.net>
2018-07-12 11:50:13 +0900

    b

---
+++ greet/delete.php
@@ -0,0 +1,19 @@
+<?php 
+
+	$num= $_GET['num'];
+
+	require "../lib/dbconn.php";
+
+	// 삭제 쿼리문
+	$sql= "DELETE FROM greet WHERE num='$num'";
+	mysqli_query($conn, $sql);
+	mysqli_close($conn);
+
+	// 다시 목록보기로 이동
+	echo ("
+		<script>
+			location.href='list.php';
+		</script>
+		");
+
+ ?>
---
+++ lib/dbconn.php
@@ -0,0 +1,3 @@
+<?php 
+	$conn= mysqli_connect("localhost", "web", "1234", "rubatto");
+ ?>
